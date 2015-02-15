c7b396f2c945af7f963dac93d9eb6d718b49fa0d
a-irs <a-irs@users.noreply.github.com>
2015-02-15 18:21:32 +0100

    iterate over multiple playlists

--- bin/lib/download-playlist.sh
+++ bin/lib/download-playlist.sh
@@ -2,28 +2,33 @@
 
 [[ -z $1 ]] && exit 1
 
-playlist=$1
-pl_name=$(basename -s .m3u "$playlist")
-outputdir="$HOME/media-gmusic"
+for playlist in "$@"; do
 
-lines=$(wc -l < "$playlist")
-songs=$((($lines-1)/2))
+    echo -e "\nDOWNLOADING '$playlist'\n"
 
-mkdir -p "$outputdir/playlists"
-rm -f "$outputdir/playlists/$pl_name.m3u"
+    pl_name=$(basename -s .m3u "$playlist")
+    outputdir="$HOME/media-gmusic"
 
-for ((song = 1; song <= $songs; song++)); do
-    title="$(awk "NR==$song*2 {print}" "$playlist" | cut -d',' -f2)"
-    link="$(awk "NR==$song*2+1 {print}" "$playlist")"
+    lines=$(wc -l < "$playlist")
+    songs=$((($lines-1)/2))
 
-    dest="$outputdir/$title.mp3"
-    if [[ ! -f "$dest" ]]; then
-        echo -e "$song/$songs\t$title"
-    	curl -o "$dest" "$link"
-        echo ""
-    else
-        echo -e "$song/$songs\t skipped: $title"
-    fi
+    mkdir -p "$outputdir/playlists"
+    rm -f "$outputdir/playlists/$pl_name.m3u"
 
-    echo "$outputdir/$title.mp3" >> "$outputdir/playlists/$pl_name.m3u"
+    for ((song = 1; song <= $songs; song++)); do
+        title="$(awk "NR==$song*2 {print}" "$playlist" | cut -d',' -f2)"
+        link="$(awk "NR==$song*2+1 {print}" "$playlist")"
+
+        dest="$outputdir/$title.mp3"
+        if [[ ! -f "$dest" ]]; then
+            echo -e "$song/$songs\t$title"
+        	curl -o "$dest" "$link"
+            echo ""
+        else
+            echo -e "$song/$songs\t skipped: $title"
+        fi
+
+        echo "$outputdir/$title.mp3" >> "$outputdir/playlists/$pl_name.m3u"
+    done
+
 done
