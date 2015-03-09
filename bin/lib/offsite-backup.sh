4b475e6dfc187bca5435754ab24930853507616a
a-irs <a-irs@users.noreply.github.com>
2015-03-09 15:00:49 +0100

    offsite

--- bin/lib/offsite-backup.sh
+++ bin/lib/offsite-backup.sh
@@ -4,8 +4,8 @@ set -e
 
 send_backup() {
     [ "$(ls -A /tmp/offsite-backup/$1)" ] || exit 1
-    rsync -axz --delete --numeric-ids -e 'ssh -p 5522' "/media/data/backups/host/$1/.encfs6.xml" "alex@zshine.net:backups/$1"
     rsync -axz --delete --numeric-ids -e 'ssh -p 5522' "/tmp/offsite-backup/$1" alex@zshine.net:backups
+    scp -P 5522 "/media/data/backups/host/$1/.encfs6.xml" "alex@zshine.net:backups/$1/encfs6.xml"
 }
 
 send_backup dell
