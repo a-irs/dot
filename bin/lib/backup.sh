97024c07d72bceeb5869a1b65f6c34353662bba6
a-irs <a-irs@users.noreply.github.com>
2015-03-10 11:25:35 +0100

    back.sh

--- bin/lib/backup.sh
+++ bin/lib/backup.sh
@@ -7,6 +7,7 @@ destination="root@srv:/media/data/backups/host/$HOSTNAME"
 pacman -Qqe | sort > /tmp/packages.txt && scp /tmp/packages.txt "$destination" && rm -f /tmp/packages.txt
 
 rsync -axH --delete --delete-excluded --stats --progress --human-readable --numeric-ids --info=progress2 \
+--rsync-path="sudo rsync" \
 --exclude 'dev/android' \
 --exclude 'dev/venvs' \
 --exclude 'dev/virtualbox' \
@@ -18,6 +19,7 @@ rsync -axH --delete --delete-excluded --stats --progress --human-readable --nume
 --exclude '.mozilla/firefox/*/storage/temporary' \
 --exclude '.mozilla/firefox/*/sessionstore-backups' \
 --exclude '**/compile-cache' \
+--exclude '**/Package Control.cache' \
 --exclude '**/Cache' \
 --exclude '**/cache' \
 --exclude '**/backup' \
@@ -41,6 +43,7 @@ rsync -axH --delete --delete-excluded --stats --progress --human-readable --nume
 "$HOME" "$destination/home"
 
 sudo rsync -axH --delete --delete-excluded --stats --progress --human-readable --numeric-ids --info=progress2 \
+--rsync-path="sudo rsync" \
 --exclude 'udev/hwdb.bin' \
 --exclude 'ld.so.cache' \
 --exclude '*.pacnew' \
@@ -50,6 +53,7 @@ sudo rsync -axH --delete --delete-excluded --stats --progress --human-readable -
 /etc "$destination"
 
 sudo rsync -axH --delete --delete-excluded --stats --progress --human-readable --numeric-ids --info=progress2 \
+--rsync-path="sudo rsync" \
 --exclude '.cache/*' \
 --exclude '.git-credentials' \
 /root "$destination"
