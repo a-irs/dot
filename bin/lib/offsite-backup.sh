ec2d3bb751018cd3ca3e392f81dcd6ded9c86267
a-irs <a-irs@users.noreply.github.com>
2015-03-10 13:27:39 +0100

    update

--- bin/lib/offsite-backup.sh
+++ bin/lib/offsite-backup.sh
@@ -9,7 +9,6 @@ send_backup() {
 
     rsync -a --delete --numeric-ids "/media/data/backups/host/$1/" "$tmp/$1"
     rsync -a --compress --delete --numeric-ids -e "ssh -p 5522" "$tmp/$1-encrypted" alex@zshine.net:backups
-    rsync -a --compress --delete --numeric-ids -e "ssh -p 5522" "$tmp/$1-ecryptfs" alex@zshine.net:backups
 }
 
 send_backup dell
