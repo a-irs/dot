fa624f81bcbeaa5e59a615161ede262d144c80df
a-irs <a-irs@users.noreply.github.com>
2015-03-10 13:08:05 +0100

    update

--- bin/lib/offsite-backup.sh
+++ bin/lib/offsite-backup.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-send_backup() {
-    mount | grep "on /var/tmp/offsite-backup/$1" || return 1
-    [[ $? == 1 ]] && return 1
+tmp=/var/tmp/offsite-backup
 
-    rsync -a --delete --numeric-ids "/media/data/backups/host/$1/" "/var/tmp/offsite-backup/$1"
-    rsync -a --compress --delete --numeric-ids -e "ssh -p 5522" "/var/tmp/offsite-backup/$1-encrypted" alex@zshine.net:backups
-    rsync -a --compress --delete --numeric-ids -e "ssh -p 5522" "/var/tmp/offsite-backup/$1-ecryptfs" alex@zshine.net:backups
+send_backup() {
+    mount | grep "on $tmp/$1" || return 1
+
+    rsync -a --delete --numeric-ids "/media/data/backups/host/$1/" "$tmp/$1"
+    rsync -a --compress --delete --numeric-ids -e "ssh -p 5522" "$tmp/$1-encrypted" alex@zshine.net:backups
+    rsync -a --compress --delete --numeric-ids -e "ssh -p 5522" "$tmp/$1-ecryptfs" alex@zshine.net:backups
 }
 
 send_backup dell
--- bin/lib/setup-ecryptfs.sh
+++ bin/lib/setup-ecryptfs.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+tmp=/var/tmp/offsite-backup
+
 set_up() {
-    mkdir -p -m 700 /var/tmp/offsite-backup/${1}-encrypted /var/tmp/offsite-backup/${1}-ecryptfs
-    mkdir -p -m 500 /var/tmp/offsite-backup/$1
-    echo "/var/tmp/offsite-backup/${1}-encrypted /var/tmp/offsite-backup/${1}-ecryptfs" > /var/tmp/offsite-backup/${1}-ecryptfs/secret.conf
-    [ -f /var/tmp/offsite-backup/${1}-ecryptfs/secret.sig ] || bash -c 'read -s -e -r -p "Passphrase: " tmp; echo $tmp | ecryptfs-add-passphrase | cut -d "[" -f 2 | cut -d "]" -f 1 | tail -n +2' > /var/tmp/offsite-backup/${1}-ecryptfs/secret.sig
-    mkdir -p ~/.ecryptfs && cp -f /var/tmp/offsite-backup/${1}-ecryptfs/secret.sig ~/.ecryptfs/sig-cache.txt
-    
-    mount | grep "on /var/tmp/offsite-backup/$1" && return 1
-    mount -t ecryptfs -o rw,nosuid,nodev,relatime,ecryptfs_sig=$(cat /var/tmp/offsite-backup/${1}-ecryptfs/secret.sig),ecryptfs_cipher=aes,ecryptfs_key_bytes=16,ecryptfs_unlink_sigs /var/tmp/offsite-backup/${1}-encrypted /var/tmp/offsite-backup/$1
+    mkdir -p -m 700 $tmp/${1}-encrypted $tmp/${1}-ecryptfs
+    mkdir -p -m 500 $tmp/$1
+    echo "$tmp/${1}-encrypted $tmp/${1}-ecryptfs" > $tmp/${1}-ecryptfs/secret.conf
+    [ -f $tmp/${1}-ecryptfs/secret.sig ] || read -s -e -r -p "Passphrase: " t; echo "$t" | ecryptfs-add-passphrase | cut -d "[" -f 2 | cut -d "]" -f 1 | tail -n +2 > $tmp/${1}-ecryptfs/secret.sig
+    mkdir -p ~/.ecryptfs && cp -f $tmp/${1}-ecryptfs/secret.sig ~/.ecryptfs/sig-cache.txt
+
+    mount | grep "on $tmp/$1" && return 1
+    mount -t ecryptfs -o rw,nosuid,nodev,relatime,ecryptfs_sig="$(cat $tmp/${1}-ecryptfs/secret.sig)",ecryptfs_cipher=aes,ecryptfs_key_bytes=16,ecryptfs_unlink_sigs $tmp/${1}-encrypted $tmp/$1
 }
 
 set_up dell
