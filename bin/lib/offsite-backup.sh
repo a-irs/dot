1f8e3b0c7b05458a9c1613afc09a4a1bb81ece38
a-irs <a-irs@users.noreply.github.com>
2015-03-10 12:33:22 +0100

    update

--- bin/lib/offsite-backup.sh
+++ bin/lib/offsite-backup.sh
@@ -3,9 +3,13 @@
 set -e
 
 send_backup() {
-    [ "$(ls -A /tmp/offsite-backup/$1)" ] || return 1
-    rsync -axzv --delete --numeric-ids -e 'ssh -p 5522' "/tmp/offsite-backup/$1" alex@zshine.net:backups
-    scp -P 5522 "/media/data/backups/host/$1/.encfs6.xml" "alex@zshine.net:backups/$1/encfs6.xml"
+    mount | grep "on /tmp/offsite-backup/$1" || return 1
+    [[ $? == 1 ]] && return 1
+
+    rsync -a --delete --numeric-ids "/media/data/backups/host/$1/" "/tmp/offsite-backup/$1"
+    rsync -a --compress --delete --numeric-ids "/tmp/offsite-backup/$1-encrypted" alex@zshine.net:backups
+    rsync -a --compress --delete --numeric-ids "/tmp/offsite-backup/$1-ecryptfs" alex@zshine.net:backups
+    scp "/media/backups/$1/.encfs6.xml" "alex@zshine.net:backups/$1/encfs6.xml"
 }
 
 send_backup dell
