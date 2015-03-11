8530ec6205113500924c3fe0eebcafc895c682a9
a-irs <a-irs@users.noreply.github.com>
2015-03-11 18:50:52 +0100

    security measures for root

--- bin/lib/backup.sh
+++ bin/lib/backup.sh
@@ -17,7 +17,7 @@ backup() {
     src=$1
     dst=$2
     header 4 "backing up $src to $dst"
-    if [[ "$3" == sudo ]]; then
+    if [[ "$3" == sudo && $UID != 0 ]]; then
         sudo -E duplicity --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-globbing-filelist="$(dirname "$(readlink -f "$0")")/backup.exclude" "$src" "$dst"
     else
         duplicity --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-globbing-filelist="$(dirname "$(readlink -f "$0")")/backup.exclude" "$src" "$dst"
@@ -27,6 +27,6 @@ backup() {
 header 4 "sending package list to $destination_ssh/packages.txt"
 pacman -Qqe | sort > /tmp/packages.txt && scp /tmp/packages.txt "$destination_ssh/packages.txt" && rm -f /tmp/packages.txt
 
-backup "$HOME" "$destination/home"
+[[ $HOME != /root ]] && backup "$HOME" "$destination/home"
 backup "/etc" "$destination/etc" sudo
 backup "/root" "$destination/root" sudo
