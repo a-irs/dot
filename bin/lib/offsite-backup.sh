af2cab8d2363f4fb1d2e0b31023321c6b8fe6b54
a-irs <a-irs@users.noreply.github.com>
2015-03-12 15:20:05 +0100

    add offsite-backup

---
+++ bin/lib/offsite-backup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rsync -rv --delete -e "ssh -p 5522" /media/data/backups/duplicity/ alex@zshine.net:backups/duplicity
+
+export PASSPHRASE=$(cat /duplicity-passphrase)
+duplicity --archive-dir=/var/tmp/duplicity /media/data/backups/host scp://alex@zshine/backups/host
+duplicity --archive-dir=/var/tmp/duplicity /media/data/backups/sql scp://alex@zshine/backups/sql
+duplicity --archive-dir=/var/tmp/duplicity /media/data/backups/privat scp://alex@zshine/backups/privat
+duplicity --archive-dir=/var/tmp/duplicity /media/data/backups/studium scp://alex@zshine/backups/studium
---
+++ bin/srv/offsite-backup.sh
@@ -0,0 +1,1 @@
+../lib/offsite-backup.sh
