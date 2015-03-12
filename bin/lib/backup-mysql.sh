0cc51b8ea0a7ac6d0fc41111125e363c100c2c78
a-irs <a-irs@users.noreply.github.com>
2015-03-12 18:37:52 +0100

    update backup-mysql (exit if not reachable)

--- bin/lib/backup-mysql.sh
+++ bin/lib/backup-mysql.sh
@@ -4,4 +4,5 @@ set -e
 
 mkdir -p /media/data/backups/sql
 DEST="/media/data/backups/sql/srv_kodi_$(date +%Y-%m-%d_%H-%M).sql.gz"
-mysqldump -h 127.0.0.1 -u xbmc -pGtquwKYtYdueTbmI --all-databases | gzip > "$DEST"
+o=$(mysqldump -h 127.0.0.1 -u xbmc -pGtquwKYtYdueTbmI --all-databases 2> /dev/null)
+[[ -n "$o" ]] && echo "$o" | gzip > "$DEST"
