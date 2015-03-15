1330395ae4d6a20f59a9f11989e56b16e32e91eb
a-irs <a-irs@users.noreply.github.com>
2015-03-15 15:12:29 +0100

    add pogo to ssh-config, modify sshuttle for zshine

--- bin/lib/sshuttle.sh
+++ bin/lib/sshuttle.sh
@@ -2,10 +2,13 @@
 
 pid=/tmp/sshuttle.pid
 
-sudo sshuttle --dns -r alexi@aquila.uberspace.de 0/0 \
+echo "before: $(curl -s icanhazip.com)"
+sudo sshuttle -e 'ssh -i /home/alex/.ssh/id_rsa' --dns -r alex@zshine.net:5522 0/0 \
     -x 192.168.2.0/24 -x 10.0.0.0/24 -x 10.0.1.0/24 \
-    -D --pidfile="$pid" --no-latency-control
+    -D --pidfile="$pid" --no-latency-control > /dev/null
+sleep 1
+echo "after:  $(curl -s icanhazip.com)"
 
-echo [ENTER TO STOP]
+echo -e "\n[ENTER TO STOP]"
 read
-[ -f "$pid" ] && sudo kill $(cat "$pid") && sudo rm -f "$pid"
+[ -f "$pid" ] && sudo kill "$(cat "$pid")" && sudo rm -f "$pid"
