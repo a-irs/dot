14a6ad9d21d46c4e3aa0bec31588d817f6f39f90
a-irs <a-irs@users.noreply.github.com>
2015-03-31 16:43:55 +0200

    update fzf

--- zshine/plugins/fzf.zsh
+++ zshine/plugins/fzf.zsh
@@ -1,19 +1,34 @@
 alias fzf="ruby $ZSHINE_DIR/plugins/fzf/fzf --extended-exact --no-256"
 
 __fsel() {
-    set -o nonomatch
-    command find * \
-           -type f -print \
-        -o -type d -print \
-        -o -type l -print 2> /dev/null | fzf -m | while read i; do
-            printf '%q ' "$i"
-        done
+    command find -L . \
+    \( -fstype 'dev' -o -fstype 'proc' \) -prune \
+    -o -type f -print \
+    -o -type d -print \
+    -o -type l -print 2> /dev/null | sed 1d | cut -b3- | fzf -m | while read item; do
+        printf '%q ' "$item"
+    done
     echo
 }
 
+# CTRL-T - Paste the selected file path(s) into the command line
 fzf-file-widget() {
     LBUFFER="${LBUFFER}$(__fsel)"
     zle redisplay
 }
 zle     -N   fzf-file-widget
 bindkey '^T' fzf-file-widget
+
+# CTRL-G - cd into the selected directory
+fzf-cd-widget() {
+    old_chpwd=$(which chpwd 2> /dev/null)
+    unfunction chpwd
+    cd "${$(command find -L . \
+        \( -fstype 'dev' -o -fstype 'proc' \) -prune \
+        -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m):-.}"
+    eval "$old_chpwd"
+    unset "$old_chpwd"
+    zle reset-prompt
+}
+zle     -N    fzf-cd-widget
+bindkey '^G' fzf-cd-widget
