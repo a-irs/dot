a17c977620a1074c42d88b938c8d63c0fd3f151d
a-irs <a-irs@users.noreply.github.com>
2015-04-08 13:53:53 +0200

    refine rsync-data for srv

--- bin/lib/rsync-data.sh
+++ bin/lib/rsync-data.sh
@@ -17,13 +17,13 @@ rsync_launch() {
 	echo
 	echo '########################################'
 	echo
-	rsync -av --delete --stats --log-file=$3/$2.log --backup-dir=$3/old/$2 $1/$2 $3 --exclude backups/host
+	rsync -av --delete --stats --log-file="$3/$2.log" --backup-dir="$3/__archive/$2" "$1/$2" "$3"
 }
 
 control_c() {
   echo -en "\n*** CTRL+C - starting cleanup ***\n"
   sync
-  umount -lf $DEST
+  umount -lf "$DEST"
   sync
   exit $?
 }
@@ -31,16 +31,16 @@ control_c() {
 trap control_c SIGINT
 
 delay=0.3
-mount $DEST
-if [ $? -eq 0 ]; then
+mount "$DEST"
+if [ "$?" -eq 0 ]; then
 	echo -e '\a' > /dev/console
-	rsync_launch $SRC photos $DEST
-	rsync_launch $SRC music $DEST
-	rsync_launch $SRC backups $DEST
-	rsync_launch $SRC books $DEST
-	rsync_launch /    srv $DEST
+	rsync_launch "$SRC" photos "$DEST"
+	rsync_launch "$SRC" music "$DEST"
+	rsync_launch "$SRC" books "$DEST"
+	rsync_launch "$SRC" virtualbox "$DEST"
+	rsync_launch "$SRC" games "$DEST"
 	sync
-	umount -lf $DEST
+	umount -lf "$DEST"
 	sleep 5s
 	sync
 	echo -e '\a' > /dev/console ; sleep $delay
