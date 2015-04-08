caf7b1de456050d6baff84e0e6cfc387a7acae48
a-irs <a-irs@users.noreply.github.com>
2015-04-08 19:55:59 +0200

    backup

--- bin/lib/backup.sh
+++ bin/lib/backup.sh
@@ -12,33 +12,32 @@ fi
 destination="scp://root@srv//media/data/backups/duplicity/$HOSTNAME"
 destination_ssh="root@srv:/media/data/backups/duplicity/$HOSTNAME"
 archive_dir="/var/tmp/duplicity"
+excludes="$(dirname "$(readlink -f "$0")")/backup.exclude"
 
 header() {
     echo -e "\n$(tput setaf "${1}";tput bold)${2}$(tput init)\n"
 }
 
-backup() {
-    src=$1
-    dst=$2
-    extra_options=$3
-    header 4 "backing up $src to $dst"
-    if [[ -n "$extra_options" ]]; then
-        eval $(echo sudo -E duplicity --archive-dir="$archive_dir" --exclude-other-filesystems $(echo $extra_options) "$src" "$dst")
-    else
-        sudo -E duplicity --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-globbing-filelist="$(dirname "$(readlink -f "$0")")/backup.exclude" "$src" "$dst"
-    fi
-}
+header 4 "backing up '/home' to '$destination/home'"
+sudo -E duplicity --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-globbing-filelist="$excludes" "/home" "$destination/home"
 
-backup /home "$destination/home"
-backup /etc "$destination/etc"
-backup /root "$destination/root"
+header 4 "backing up '/etc' to '$destination/etc'"
+sudo -E duplicity --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-globbing-filelist="$excludes" "/etc" "$destination/etc"
+
+header 4 "backing up '/root' to '$destination/root'"
+sudo -E duplicity --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-globbing-filelist="$excludes" "/root" "$destination/root"
 
 if [[ $HOSTNAME == srv ]]; then
-    backup /srv "$destination/srv" "--include /srv/smb --include /srv/http --include /srv/docker/sabnzbd/state/sabnzbd.ini --exclude '**'"
-    backup /var/spool/cron "$destination/cron"
+    header 4 "backing up '/srv to '$destination/srv'"
+    sudo -E duplicity --archive-dir="$archive_dir" --include /srv/smb --include /srv/http --include /srv/docker/sabnzbd/state/sabnzbd.ini --exclude '**' "/srv" "$destination/srv"
+
+    header 4 "backing up '/var/spool/cron' to '$destination/cron'"
+    sudo -E duplicity --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-globbing-filelist="$excludes" "/var/spool/cron" "$destination/cron"
 fi
+
 unset PASSPHRASE
 
-header 5 "sending package list to $destination_ssh/$(date "+%Y-%m-%d_%H-%M")_packages.txt"
-pacman -Qe | sort > /tmp/packages.txt && scp /tmp/packages.txt "$destination_ssh/$(date "+%Y-%m-%d_%H-%M")_packages.txt" && rm -f /tmp/packages.txt
-
+header 5 "sending package list to '$destination_ssh/$(date "+%Y-%m-%d_%H-%M")_packages.txt'"
+pacman -Qe | sort > /tmp/packages.txt
+scp /tmp/packages.txt "$destination_ssh/$(date "+%Y-%m-%d_%H-%M")_packages.txt" > /dev/null
+rm -f /tmp/packages.txt
