f1f1f5f44e9d332964b308a2d9c1786d4a65a992
a-irs <a-irs@users.noreply.github.com>
2015-04-14 19:23:18 +0200

    update mysql backup to delete old backups

--- bin/lib/backup-mysql.sh
+++ bin/lib/backup-mysql.sh
@@ -2,7 +2,16 @@
 
 set -e
 
+CDATE=$(date +%Y-%m-%d)
+CTIME=$(date +%H-%M)
+CSEC=$(date +%s)
+
+# backup current state
 mkdir -p /media/data/backups/sql
-DEST="/media/data/backups/sql/srv_kodi_$(date +%Y-%m-%d_%H-%M).sql.gz"
+DEST="/media/data/backups/sql/srv_kodi_${CDATE}_${CTIME}.sql.gz"
 o=$(mysqldump -h 127.0.0.1 -u xbmc -pGtquwKYtYdueTbmI --all-databases 2> /dev/null)
 [[ -n "$o" ]] && echo "$o" | gzip > "$DEST"
+
+# delete backups older than 7d
+limit=$(date --date=@$((CSEC-60*60*24*7)) +%Y-%m-%d)
+find /media/data/backups/sql -name "srv_kodi_*" | sort -h | grep -B 10000 "$limit" | xargs -L1 rm -f
