5c24c892dbcd45a1f2aeb19e464b8251f5fe6be4
a-irs <a-irs@users.noreply.github.com>
2015-05-01 14:08:09 +0200

    x2go-genmon fix

--- bin/lib/genmon/x2go.sh
+++ bin/lib/genmon/x2go.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep x2goagent > /dev/null; then
-   echo -n "<txt><span weight='bold' fgcolor='Red'> CONNECTED </span></txt>"
+    echo -n "<txt><span weight='bold' fgcolor='Red'> CONNECTED </span></txt>"
+else
+    echo -n "<txt></txt>"
 fi
--- bin/lib/toggle-res.sh
+++ bin/lib/toggle-res.sh
@@ -4,12 +4,14 @@ OUTPUT="DVI-1"
 MODE_NATIVE="1920x1200"
 MODE_COUCH="1280x800"
 
-CURRENT=$(xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}')
+[[ -n "$1" ]] && DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$1" && exit
+
+CURRENT=$(DISPLAY=:0 xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}')
 
 if [[ "$CURRENT" == "$MODE_NATIVE" ]]; then
-    xrandr --output "$OUTPUT" --mode "$MODE_COUCH"
+    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE_COUCH"
     echo "changed $OUTPUT to $MODE_COUCH"
 else
-    xrandr --output "$OUTPUT" --mode "$MODE_NATIVE"
+    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE_NATIVE"
     echo "changed $OUTPUT to $MODE_NATIVE"
 fi
