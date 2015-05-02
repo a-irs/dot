b73f973ce9b50a3ab978069e815608627b870dcd
a-irs <a-irs@users.noreply.github.com>
2015-05-02 20:58:08 +0200

    x2go

--- bin/lib/genmon/x2go.sh
+++ bin/lib/genmon/x2go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if pgrep x2goagent > /dev/null; then
-    echo -n "<txt><span weight='bold' fgcolor='Red'> CONNECTED </span></txt>"
+if pgrep -x x2goagent > /dev/null; then
+    echo -n "<txt><span weight='bold' fgcolor='Red'> X2GO CONNECTED </span></txt>"
 else
     echo -n "<txt></txt>"
 fi
