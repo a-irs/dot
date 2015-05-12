0b2bb771ef6d14e7d7af795d8e82b5f0a365c7b7
a-irs <a-irs@users.noreply.github.com>
2015-05-12 20:49:50 +0200

    tmux

--- zshine/plugins/tmux.zsh
+++ zshine/plugins/tmux.zsh
@@ -1,7 +1,19 @@
 #!/usr/bin/env zsh
 
 # exit if tmux not installed, not starting a interactive shell or logged in to TTY
-[[ -n $SSH_TTY || -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
+[[ -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
 
-tmux attach &> /dev/null
-[[ -z "$TMUX" ]] && exec tmux
+if [[ $PS1 && $SSH_TTY && -z $TMUX && $1 != $3 && ! -r ~/.notmux ]]; then
+    if ! tmux has-session -t remote; then
+        exec tmux new-session -s remote
+    else
+        exec tmux \
+            new-session -d -s remote_$$ -t remote \;\
+            new-window \;\
+            attach \;\
+            set-option destroy-unattached on
+    fi
+else
+    tmux attach &> /dev/null
+    [[ -z "$TMUX" ]] && exec tmux
+fi
