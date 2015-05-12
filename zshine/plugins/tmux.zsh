702f7ba82650e7402e2f497ee4b1200485ba91f6
a-irs <a-irs@users.noreply.github.com>
2015-05-12 21:04:53 +0200

    tmux

--- zshine/plugins/tmux.zsh
+++ zshine/plugins/tmux.zsh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 
 # exit if tmux not installed, not starting a interactive shell or logged in to TTY
-[[ -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
+[[ -n "$TMUX" || -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
 
-tmux attach &> /dev/null
-[[ -z "$TMUX" ]] && exec tmux
+exec tmux new-session -A -s main
