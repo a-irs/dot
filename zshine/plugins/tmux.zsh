15a957cc73af5862cb9c3373b21b4a35aa3dcd5d
a-irs <a-irs@users.noreply.github.com>
2015-05-12 20:37:49 +0200

    fix tmux plugin

--- zshine/plugins/tmux.zsh
+++ zshine/plugins/tmux.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-[[ $- != *i* || "$TERM" = linux ]] && return
+# exit if tmux not installed, not starting a interactive shell or logged in to TTY
+[[ -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
 
 tmux attach &> /dev/null
 [[ -z "$TMUX" ]] && exec tmux
-
