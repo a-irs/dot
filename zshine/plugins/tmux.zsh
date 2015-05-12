2b984fcfb0cedfea16bfc80df2031d577ac8b717
a-irs <a-irs@users.noreply.github.com>
2015-05-12 20:51:12 +0200

    tmux

--- zshine/plugins/tmux.zsh
+++ zshine/plugins/tmux.zsh
@@ -3,7 +3,7 @@
 # exit if tmux not installed, not starting a interactive shell or logged in to TTY
 [[ -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
 
-if [[ $PS1 && $SSH_TTY && -z $TMUX && $1 != $3 && ! -r ~/.notmux ]]; then
+if [[ -n $SSH_TTY && -z $TMUX ]]; then
     if ! tmux has-session -t remote; then
         exec tmux new-session -s remote
     else
