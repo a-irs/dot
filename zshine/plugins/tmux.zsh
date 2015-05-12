c2bb9a84af874226d3f8d788fecfaafae590d04c
a-irs <a-irs@users.noreply.github.com>
2015-05-12 20:45:34 +0200

    dont start tmux in ssh

--- zshine/plugins/tmux.zsh
+++ zshine/plugins/tmux.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # exit if tmux not installed, not starting a interactive shell or logged in to TTY
-[[ -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
+[[ -n $SSH_TTY || -z "$commands[tmux]" || $- != *i* || "$TERM" = linux ]] && return
 
 tmux attach &> /dev/null
 [[ -z "$TMUX" ]] && exec tmux
