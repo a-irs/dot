2be8238792b714fb26f846b045d6367ca35dcb28
a-irs <a-irs@users.noreply.github.com>
2015-05-26 19:45:09 +0200

    cleanup

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -10,16 +10,6 @@ theme.bg_systray                    = theme.bg_normal
 theme.fg_normal                     = "#aaaaaa"
 theme.fg_focus                      = "#add8e6"
 theme.fg_urgent                     = "#af1d18"
-theme.fg_minimize                   = "#ffffff"
-theme.fg_black                      = "#424242"
-theme.fg_red                        = "#ce5666"
-theme.fg_green                      = "#80a673"
-theme.fg_yellow                     = "#ffaf5f"
-theme.fg_blue                       = "#7788af"
-theme.fg_magenta                    = "#94738c"
-theme.fg_cyan                       = "#778baf"
-theme.fg_white                      = "#aaaaaa"
-theme.fg_blu                        = "#8ebdde"
 
 theme.border_width                  = 0
 theme.border_normal                 = "#1c2022"
