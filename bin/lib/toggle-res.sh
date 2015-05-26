bca1175c855130254062942bf429e3d6dae42584
a-irs <a-irs@users.noreply.github.com>
2015-05-26 23:26:38 +0200

    toggle-res

--- bin/lib/toggle-res.sh
+++ bin/lib/toggle-res.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+set -e
+
+[[ $HOSTNAME == desktop ]] || exit
+
 OUTPUT="DVI-1"
-MODE_NATIVE="1920x1200"
-MODE_COUCH="1280x800"
+MODE1="1920x1200"
+MODE2="1440x900"
+MODE3="1280x800"
 
 [[ -n "$1" ]] && DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$1" && exit
 
 CURRENT=$(DISPLAY=:0 xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}')
 
-if [[ "$CURRENT" == "$MODE_NATIVE" ]]; then
-    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE_COUCH"
-    echo "changed $OUTPUT to $MODE_COUCH"
+if [[ "$CURRENT" == "$MODE1" ]]; then
+    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE2"
+    echo "changed $OUTPUT to $MODE2"
+elif [[ "$CURRENT" == "$MODE2" ]]; then
+    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE3"
+    echo "changed $OUTPUT to $MODE3"
 else
-    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE_NATIVE"
-    echo "changed $OUTPUT to $MODE_NATIVE"
+    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE1"
+    echo "changed $OUTPUT to $MODE1"
 fi
