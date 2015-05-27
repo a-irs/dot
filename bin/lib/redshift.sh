25eb1647cf161b9232c37cdaf555d10f427a5031
a-irs <a-irs@users.noreply.github.com>
2015-05-27 20:21:00 +0200

    refine autostart

--- bin/lib/redshift.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-pgrep -x redshift || redshift
--- config/awesome/awm-autostart.lua
+++ config/awesome/awm-autostart.lua
@@ -1,25 +1,16 @@
 local programs = {
    'nitrogen --restore',
-   os.getenv("HOME") .. '/.bin/redshift.sh',
    'start-pulseaudio-x11',
    'kupfer --no-splash',
    'compton -b',
    'dropbox-cli start',
    'mpd',
-   'bash -c "pgrep lxpolkit || lxpolkit"',
+   'bash -c "pgrep -x lxpolkit || lxpolkit"',
+   'bash -c "pgrep -x redshift || redshift"',
+   hostname == "desktop" and 'bash -c "pgrep x2godesktopshar || x2godesktopsharing"',
+   hostname == "desktop" and os.getenv("HOME") .. '/.bin/wait-for-srv.sh gvfs-mount smb://srv/data',
+   hostname == "desktop" and os.getenv("HOME") .. '/.bin/wait-for-srv.sh gvfs-mount sftp://root@srv',
 }
 for _, cmd in ipairs(programs) do
     os.execute(cmd .. ' &')
-end
-
--- start programs for desktop
-local programs_desktop = {
-    'bash -c "pgrep x2godesktopshar || x2godesktopsharing"',
-    os.getenv("HOME") .. '/.bin/wait-for-srv.sh gvfs-mount smb://srv/data',
-    os.getenv("HOME") .. '/.bin/wait-for-srv.sh gvfs-mount sftp://root@srv',
-}
-if hostname == "desktop" then
-    for _, cmd in ipairs(programs_desktop) do
-        os.execute(cmd .. ' &')
-    end
 end
