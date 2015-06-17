431d68c984e67992e9b3756c3f201088557afb17
a-irs <a-irs@users.noreply.github.com>
2015-06-17 18:51:40 +0200

    add number of failed attempts to i3lock

--- bin/lib/screen-lock.sh
+++ bin/lib/screen-lock.sh
@@ -7,7 +7,7 @@ scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 [[ -n "$icon" ]] && convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 
-i3lock -e -i "$tmpbg"
+i3lock -e -i "$tmpbg" -f
 
 (($# == 0)) && exit
 
