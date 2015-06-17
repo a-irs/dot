71b100913dba863b442772e4060daa8ac88b0ef4
a-irs <a-irs@users.noreply.github.com>
2015-06-17 18:49:09 +0200

    remove icon from lock

--- bin/lib/screen-lock.sh
+++ bin/lib/screen-lock.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-icon=~/.bin/lib/screen-lock.png
+#icon=~/.bin/lib/screen-lock.png
 tmpbg=/tmp/screen.png
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+[[ -n "$icon" ]] && convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 
 i3lock -e -i "$tmpbg"
 
