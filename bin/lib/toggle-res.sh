dbb1e058c7f37902fff514384cd2f533e16a4f72
a-irs <a-irs@users.noreply.github.com>
2015-06-21 13:13:17 +0200

    toggle-res → 1024x640

--- bin/lib/toggle-res.sh
+++ bin/lib/toggle-res.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-set -e
-
 [[ $HOSTNAME == desktop ]] || exit
 
 OUTPUT="DVI-1"
 MODE1="1920x1200"
-MODE2="1440x900"
-MODE3="1280x800"
+MODE2="1024x640"
 
 [[ -n "$1" ]] && DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$1" && exit
+
+modeline=$(cvt 1024 640 | grep Modeline)
+modeline=${modeline/Modeline/}
+modeline=${modeline/_60.00/}
+mode=$(echo "$modeline" | cut -d" " -f 2)
+xrandr --newmode $modeline
+xrandr --addmode "$OUTPUT" "$mode"
 
 CURRENT=$(DISPLAY=:0 xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}')
 
 if [[ "$CURRENT" == "$MODE1" ]]; then
     DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE2"
     echo "changed $OUTPUT to $MODE2"
-elif [[ "$CURRENT" == "$MODE2" ]]; then
-    DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE3"
-    echo "changed $OUTPUT to $MODE3"
 else
     DISPLAY=:0 xrandr --output "$OUTPUT" --mode "$MODE1"
     echo "changed $OUTPUT to $MODE1"
