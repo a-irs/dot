b5c7ef0cd1422d4ec6cac808f5b2eed67bdba052
a-irs <a-irs@users.noreply.github.com>
2015-07-21 23:32:42 +0200

    change gitconfig, increase max directory count for cdr to 31

--- zshine/setup.zsh
+++ zshine/setup.zsh
@@ -30,6 +30,7 @@ zle -N edit-command-line
 autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
 add-zsh-hook chpwd chpwd_recent_dirs
 zstyle ':chpwd:*' recent-dirs-file ~/.zsh_recent-dirs +
+zstyle ':chpwd:*' recent-dirs-max 31
 
 zstyle ':completion:*' rehash true # always rehash on completion
 
