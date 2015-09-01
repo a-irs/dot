6ffc9d678a22fd64c8836ead0d22c2330f0a5c38
a-irs <a-irs@users.noreply.github.com>
2015-09-01 12:40:34 +0200

    mpd

---
+++ bin/lib/mpd-playlists.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+
+set -e
+setopt extended_glob
+
+cd ~/music/playlists
+sort -fu -- *.m3u\
+~_all.m3u\
+~*0s\ Rap.m3u\
+~African.m3u\
+~Atari.m3u\
+~Blues.m3u\
+~Corey\ Taylor.m3u\
+~Downtempo.m3u\
+~Guardians*\
+~Hard.m3u\
+~Herr\ von\ Grau.m3u\
+~Hotline\ Miami.m3u\
+~Instrumental.m3u\
+~Minus.m3u\
+~Palindrome*\
+~Redemption.m3u\
+~Run\ The\ Jewels.m3u\
+~Running.m3u\
+~Spaghetti.m3u\
+~Stones\ Throw.m3u\
+~Surf.m3u\
+~The\ Bees\ Made*.m3u\
+~Todo.m3u\
+~Unsortiert*.m3u\
+> ~/music/playlists/000_ALL.m3u
+
+find ~/music/albums | grep -E ".mp3$" | sort -fu \
+> ~/music/playlists/000_ALBUMS.m3u
---
+++ bin/mpd-playlists.sh
@@ -0,0 +1,1 @@
+lib/mpd-playlists.sh
