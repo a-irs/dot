7f6918a1b2b0d5a1039adb872b8074d8109dc255
a-irs <a-irs@users.noreply.github.com>
2015-09-03 13:40:15 +0200

    awm: make bars transparent

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -12,8 +12,8 @@ theme.statusbar_position            = "top"
 
 -- FG, BG COLORS
 
-theme.bg_normal                     = "#2c3643"
-theme.bg_focus                      = "#3c4b5d"
+theme.bg_normal                     = "#2c3643f3"
+theme.bg_focus                      = "#3c4b5df3"
 theme.bg_urgent                     = "#aa0000"
 theme.fg_normal                     = "#aaaaaa"
 theme.fg_focus                      = "#ffffff"
