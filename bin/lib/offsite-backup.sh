79991aa8548cb0c5648c09bb77501d16dfe40f2e
a-irs <a-irs@users.noreply.github.com>
2015-09-13 19:23:33 +0200

    offsite-backup

--- bin/lib/offsite-backup.sh
+++ bin/lib/offsite-backup.sh
@@ -6,9 +6,9 @@ b=/media/data/backups
 
 rsync --size-only -rv --delete -e "ssh -p 5522" "$b/duplicity" alex@zshine.net:backups
 
-export PASSPHRASE=$(cat /duplicity-passphrase)
-duplicity --archive-dir=/var/tmp/duplicity "$b/host" scp://alex@zshine.net:5522/backups/host
-duplicity --archive-dir=/var/tmp/duplicity "$b/sql" scp://alex@zshine.net:5522/backups/sql
-duplicity --archive-dir=/var/tmp/duplicity "$b/privat" scp://alex@zshine.net:5522/backups/privat
-duplicity --archive-dir=/var/tmp/duplicity "$b/studium" scp://alex@zshine.net:5522/backups/studium
-unset PASSPHRASE
+#export PASSPHRASE=$(cat /duplicity-passphrase)
+#duplicity --archive-dir=/var/tmp/duplicity "$b/host" scp://alex@zshine.net:5522/backups/host
+#duplicity --archive-dir=/var/tmp/duplicity "$b/sql" scp://alex@zshine.net:5522/backups/sql
+#duplicity --archive-dir=/var/tmp/duplicity "$b/privat" scp://alex@zshine.net:5522/backups/privat
+#duplicity --archive-dir=/var/tmp/duplicity "$b/studium" scp://alex@zshine.net:5522/backups/studium
+#unset PASSPHRASE
