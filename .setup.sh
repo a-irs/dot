6b714feb1c3cf8865161c2b20024ca468ab891d7
a-irs <a-irs@users.noreply.github.com>
2015-09-27 01:12:36 +0200

    a

--- .setup.sh
+++ .setup.sh
@@ -91,9 +91,9 @@ else
     for fx in "${dotfiles_x[@]}"; do rmlink "$fx"; done
 fi
 
-#git clone --depth=1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2> /dev/null
-#vim +PluginInstall +qall
-#print_info "installed vundles"
+git clone --depth=1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2> /dev/null
+vim +PluginInstall +qall
+print_info "installed vundles"
 
 if [[ -f /usr/bin/kupfer ]]; then
     mkdir -p ~/.local/share/kupfer/plugins
