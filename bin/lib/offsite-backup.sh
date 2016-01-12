a0542c53a95422da9418cee7f0081a58cb4a15e6
a-irs <a-irs@users.noreply.github.com>
2016-01-12 12:01:57 +0100

    offsite

--- bin/lib/offsite-backup.sh
+++ bin/lib/offsite-backup.sh
@@ -9,11 +9,13 @@ rsync --size-only -rv --delete -e "ssh -p 5522" "$b/duplicity" alex@zshine.net:b
 
 export PASSPHRASE=$(cat /duplicity-passphrase)
 
-duplicity --full-if-older-than 1M --archive-dir="$archive_dir" --exclude-other-filesystems "$b/host" scp://alex@zshine.net:5522/backups/host
-duplicity --archive-dir="$archive_dir" remove-older-than 1M --force scp://alex@zshine.net:5522/backups/sql
+duplicity --archive-dir="$archive_dir" --full-if-older-than 1M --exclude-other-filesystems "$b/host" scp://alex@zshine.net:5522/backups/host
+duplicity --archive-dir="$archive_dir" remove-older-than 1M    --force scp://alex@zshine.net:5522/backups/host
+duplicity --archive-dir="$archive_dir" remove-all-but-n-full 1 --force scp://alex@zshine.net:5522/backups/host
 
-duplicity --full-if-older-than 1M --archive-dir="$archive_dir" --exclude-other-filesystems "$b/sql" scp://alex@zshine.net:5522/backups/sql
-duplicity --archive-dir="$archive_dir" remove-older-than 1M --force scp://alex@zshine.net:5522/backups/sql
+duplicity --archive-dir="$archive_dir" --full-if-older-than 1M --exclude-other-filesystems "$b/sql" scp://alex@zshine.net:5522/backups/sql
+duplicity --archive-dir="$archive_dir" remove-older-than 1M    --force scp://alex@zshine.net:5522/backups/sql
+duplicity --archive-dir="$archive_dir" remove-all-but-n-full 1 --force scp://alex@zshine.net:5522/backups/sql
 
 #duplicity --archive-dir=/var/tmp/duplicity "$b/privat" scp://alex@zshine.net:5522/backups/privat
 #duplicity --archive-dir=/var/tmp/duplicity "$b/studium" scp://alex@zshine.net:5522/backups/studium
