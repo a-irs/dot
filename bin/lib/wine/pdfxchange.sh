0f7e7eaf552eaeda27e11752045fd2f993e9ab58
a-irs <a-irs@users.noreply.github.com>
2016-01-16 00:21:24 +0100

    removed wine

--- bin/lib/wine/pdfxchange.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PREFIX='/wine/pdfxchange'
-EXE="/wine/pdfxchange/drive_c/Program Files/pdfxchange/PDFXCview.exe"
-
-cd "$(dirname "$1")"
-env WINEPREFIX="$PREFIX" wine "$EXE" "$(basename "$1")"
