6d819e1e0cf6825f139df562323e2185862d9cea
a-irs <a-irs@users.noreply.github.com>
2016-01-28 15:03:04 +0100

    duplicity → borg

--- bin/lib/backup.sh
+++ bin/lib/backup.sh
@@ -2,104 +2,91 @@
 
 set -e
 
-destination="scp://root@srv//media/data/backups/duplicity/$HOSTNAME"
-destination_ssh="root@srv:/media/data/backups/duplicity/$HOSTNAME"
+if [[ $UID != 0 ]]; then
+    echo "run as root"
+    exit 1
+fi
+
+t=/0-backup-info
+
+finish() {
+    rm -rf "$t"
+}
+trap finish EXIT
+
+REPO=root@srv:/media/data/backups/borg/$HOSTNAME
+DATE=$(date +%Y-%m-%d)_$(date +%H-%M-%S)
+
+[[ -d /home ]]           && BACKUP+=( /home )
+[[ -d /etc ]]            && BACKUP+=( /etc )
+[[ -d /root ]]           && BACKUP+=( /root )
+[[ -d /srv ]]            && BACKUP+=( /srv )
+[[ -d /var/spool/cron ]] && BACKUP+=( /var/spool/cron )
+BACKUP+=( /0-backup-info )
 
 header() {
     echo -e "\n$(tput setaf "${1}";tput bold)${2}$(tput init;tput sgr0)\n"
 }
-date="$(date "+%Y-%m-%d_%H-%M")"
 
-# (UNENCRYPTED) backup package list
-
-header 5 "sending package list to '$destination_ssh/${date}_packages.txt'"
-pacman -Qe | sort > /tmp/packages.txt
-scp /tmp/packages.txt "$destination_ssh/${date}_packages.txt"
-rm -f /tmp/packages.txt
-
-
-# (UNENCRYPTED) backup partition table and LUKS header
-
-header 5 "sending storage info to '$destination_ssh'"
-device=/dev/sda
-LC_ALL=C sudo sfdisk ${device} -d > /tmp/disk-sfdisk-sda.txt
-scp /tmp/disk-sfdisk-sda.txt "$destination_ssh/${date}_disk-sfdisk-sda.txt"
-LC_ALL=C sudo fdisk -l ${device}  > /tmp/disk-fdisk-sda.txt
-scp /tmp/disk-fdisk-sda.txt "$destination_ssh/${date}_disk-fdisk-sda.txt"
-
-if [[ $HOSTNAME == dell ]]; then
-    LC_ALL=C sudo cryptsetup luksDump ${device}5 > /tmp/disk-luks-sda5.txt
-    scp /tmp/disk-luks-sda5.txt "$destination_ssh/${date}_disk-luks-sda5.txt"
-
-    sudo rm -f /tmp/disk-luks-header-sda5.img
-    sudo cryptsetup luksHeaderBackup ${device}5 --header-backup-file /tmp/disk-luks-header-sda5.img
-    sudo chmod +r /tmp/disk-luks-header-sda5.img
-    scp /tmp/disk-luks-header-sda5.img "$destination_ssh/${date}_disk-luks-header-sda5.img"
-fi
-
-sudo rm -f /tmp/disk-fdisk-sda.txt /tmp/disk-disk-sda.txt /tmp/disk-luks-sda5.txt /tmp/disk-luks-header-sda5.img
-
-
-# encrypted duplicity backup
-
-if [[ -z "$PASSPHRASE" ]]; then
-    if [[ -f /duplicity-passphrase ]]; then
-        export PASSPHRASE=$(cat /duplicity-passphrase)
+if [[ -z "$BORG_PASSPHRASE" ]]; then
+    if [[ -f /borg-passphrase ]]; then
+        export BORG_PASSPHRASE=$(< /borg-passphrase)
     else
-       read -r -s -p "Passphrase: " PASSPHRASE
-       export PASSPHRASE
+       read -r -s -p "Passphrase: " BORG_PASSPHRASE
+       echo ''
+       export BORG_PASSPHRASE
     fi
 fi
 
-archive_dir="/var/tmp/duplicity"
 excludes="$(dirname "$(readlink -f "$0")")/backup.exclude"
 
-header 4 "backing up '/home' to '$destination/home'"
-sudo -E duplicity --full-if-older-than 1M --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-filelist="$excludes" "/home" "$destination/home"
-sudo -E duplicity --archive-dir="$archive_dir" remove-older-than 1M --force "$destination/home"
-
-header 4 "backing up '/etc' to '$destination/etc'"
-sudo -E duplicity --full-if-older-than 1M --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-filelist="$excludes" "/etc" "$destination/etc"
-sudo -E duplicity --archive-dir="$archive_dir" remove-older-than 1M --force "$destination/etc"
-
-header 4 "backing up '/root' to '$destination/root'"
-sudo -E duplicity --full-if-older-than 1M --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-filelist="$excludes" "/root" "$destination/root"
-sudo -E duplicity --archive-dir="$archive_dir" remove-older-than 1M --force "$destination/root"
-
-if [[ $HOSTNAME == srv ]]; then
-    header 4 "backing up '/srv to '$destination/srv'"
-    sudo -E duplicity --full-if-older-than 1M --archive-dir="$archive_dir" --include /srv/smb --include /srv/http --include /srv/docker/sabnzbd/state/sabnzbd.ini --exclude '**' "/srv" "$destination/srv"
-    sudo -E duplicity --archive-dir="$archive_dir" remove-older-than 1M --force "$destination/srv"
-
-    header 4 "backing up '/var/spool/cron' to '$destination/cron'"
-    sudo -E duplicity --full-if-older-than 1M --archive-dir="$archive_dir" --exclude-other-filesystems --exclude-filelist="$excludes" "/var/spool/cron" "$destination/cron"
-    sudo -E duplicity --archive-dir="$archive_dir" remove-older-than 1M --force "$destination/cron"
+if [[ $1 == list ]]; then
+    borg list "$REPO"
+    exit
 fi
 
-header 2 "cleaning $destination/home"
-sudo -E duplicity --archive-dir="$archive_dir" remove-all-but-n-full 1 --force "$destination/home"
-header 2 "cleaning $destination/etc"
-sudo -E duplicity --archive-dir="$archive_dir" remove-all-but-n-full 1 --force "$destination/etc"
-header 2 "cleaning $destination/root"
-sudo -E duplicity --archive-dir="$archive_dir" remove-all-but-n-full 1 --force "$destination/root"
-if [[ $HOSTNAME == srv ]]; then
-    header 2 "cleaning $destination/srv"
-    sudo -E duplicity --archive-dir="$archive_dir" remove-all-but-n-full 1 --force "$destination/srv"
-    header 2 "cleaning $destination/cron"
-    sudo -E duplicity --archive-dir="$archive_dir" remove-all-but-n-full 1 --force "$destination/cron"
+if [[ $1 == extract ]]; then
+    borg extract --verbose "$REPO"::"$2"
+    exit
 fi
 
-unset PASSPHRASE
+if [[ $1 == info ]]; then
+    borg info --verbose "$REPO"::"$2"
+    exit
+fi
 
+header 2 "INFORMATION ABOUT BACKUP"
+echo "  - TARGET:   $(tput setaf 4)$REPO$(tput init;tput sgr0)::$DATE"
+echo "  - SOURCES:  $(tput setaf 4)${BACKUP[*]}$(tput init;tput sgr0)"
 
-# delete files older than 30 days
+header 2 "MAKING BACKUP INFO FILES in $t"
+mkdir -p $t
+echo "  - PACMAN PACKAGES"
+pacman -Qe | sort > $t/packages.txt
+echo "  - PARTITION LAYOUT OF /dev/sda"
+LC_ALL=C fdisk -l /dev/sda > $t/disk-fdisk-sda.txt
+if [[ $HOSTNAME == dell ]]; then
+    echo "  - LUKS DUMP OF /dev/sda5"
+    LC_ALL=C cryptsetup luksDump /dev/sda5 > $t/disk-luks-sda5.txt
+    echo "  - LUKS HEADER BACKUP OF /dev/sda5"
+    cryptsetup luksHeaderBackup /dev/sda5 --header-backup-file $t/disk-luks-header-sda5.img
+fi
 
-ssh root@srv bash -c "'
-limit=$(date -d "1 month ago" +%Y%m%d)
-for f in /media/data/backups/duplicity/$HOSTNAME/????-??-??_*; do
-    d=\$(basename "\$f")
-    d=\${d%%_*}
-    d=\${d//-/}
-    [[ \${d} -lt \${limit} ]] && rm -f "\$f"
-done
-'"
+header 2 "INIT REPOSITORY"
+borg init --encryption repokey "$REPO" || true
+
+header 2 "BACKING UP ${BACKUP[*]}"
+borg create --progress --stats \
+    --exclude-caches --exclude-from "$excludes" \
+    --checkpoint-interval 30 \
+    --one-file-system \
+    --compression lz4 \
+    --chunker-params 19,23,21,4095 \
+    "$REPO"::"$DATE" \
+    "${BACKUP[@]}"
+
+header 2 "PRUNING BACKUPS OLDER THAN 1 MONTH"
+borg prune --verbose --stats --keep-within 1m "$REPO"
+
+header 2 "CLEANING UP BACKUP INFO FILES in $t"
+rm -rf "$t"
