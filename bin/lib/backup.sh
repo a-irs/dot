81ba2e309ac388ec366be8e2b749208f9406844d
a-irs <a-irs@users.noreply.github.com>
2016-01-28 15:28:33 +0100

    fix

--- bin/lib/backup.sh
+++ bin/lib/backup.sh
@@ -99,7 +99,7 @@ b=/media/data/backups
 
 rsync --size-only -av --delete -e "ssh -p 5522" "$b/borg/" alex@zshine.net:backups/borg-hosts
 
-REPO=ssh://alex@zshine.net:5522/backups/borg-private
+REPO=ssh://alex@zshine.net:5522/home/alex/backups/borg-private
 DATE=$(date +%Y-%m-%d)_$(date +%H-%M-%S)
 BACKUP=( $b/host $b/studium $b/encrypt $b/encrypt-old $b/privat )
 
