b471ab482cb09cd40448bc4c172c22a92250dad7
a-irs <a-irs@users.noreply.github.com>
2016-01-29 15:40:27 +0100

    add mount-crypto

---
+++ bin/lib/mount-crypto.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+UUID=30e17b83-aaa2-4164-9248-00610b01ffed
+MOUNTPOINT=/media/crypto
+MAPPER_NAME=luks
+
+if [[ $1 == close ]]; then
+    umount $MOUNTPOINT
+    cryptsetup close $MAPPER_NAME
+    rmdir $MOUNTPOINT
+    exit
+fi
+
+mkdir $MOUNTPOINT
+cryptsetup luksOpen /dev/disk/by-uuid/$UUID $MAPPER_NAME
+mount /dev/mapper/$MAPPER_NAME $MOUNTPOINT
+cd $MOUNTPOINT
