b9ca76fdedd01b38295aba098f664b7ca59d65ae
a-irs <a-irs@users.noreply.github.com>
2016-01-29 13:39:20 +0100

    rsync-data

--- bin/lib/rsync-data.sh
+++ bin/lib/rsync-data.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
-DEST=/media/backup
-SRC=/media/data
+set -e
 
-rsync_launch() {
-	echo
-	echo '########################################'
-	echo
-	echo "starting rsync..."
-	echo
-	echo "source: $1/$2"
-	echo "target: $3/$2"
-	echo
-	echo "logfile: $3/$2.log"
-	echo "backup-dir: $3/__archive/$2/$(date +%Y-%m-%d_%H-%M-%S)/"
-	echo
-	echo '########################################'
-	echo
-	rsync -av -h --delete --stats --log-file="$3/$2.log" --backup --backup-dir="$3/__archive/$2/$(date +%Y-%m-%d_%H-%M-%S)/" "$1/$2" "$3"
-}
+TARGET=/media/usb-backup
 
 control_c() {
   echo -en "\n*** CTRL+C - starting cleanup ***\n"
   sync
-  umount -lf "$DEST"
+  umount -lf "$TARGET"
   sync
   exit $?
 }
-
 trap control_c SIGINT
 
+BACKUP=(
+	/media/data/photos
+	/media/data/music
+	/media/data/books
+	/media/data/virtualbox
+	/media/data/games
+	/media/data/videos/kids
+	/dev/disk/by-uuid/30e17b83-aaa2-4164-9248-00610b01ffed
+)
+
 delay=0.3
-mount "$DEST"
-if [ "$?" -eq 0 ]; then
+
+if mount "$TARGET"; then
 	echo -e '\a' > /dev/console
-	rsync_launch "$SRC" photos "$DEST"
-	rsync_launch "$SRC" music "$DEST"
-	rsync_launch "$SRC" books "$DEST"
-	rsync_launch "$SRC" virtualbox "$DEST"
-	rsync_launch "$SRC" backups "$DEST"
-	rsync_launch "$SRC" games "$DEST"
+
+	for d in "${BACKUP[@]}"; do
+		date=$(date +%Y-%m-%d)_$(date +%H-%M-%S)
+		repo=$TARGET/"$(basename "$d")"
+
+		borg init --encryption none "$repo" || true
+		borg create --progress --stats --verbose \
+		    --one-file-system \
+	    	--compression lz4 \
+	    	--chunker-params 19,23,21,4095 \
+	    	"$repo"::"$date" \
+	    	"$d"
+	done
+
 	sync
-	umount -lf "$DEST"
+	umount -lf "$TARGET"
 	sleep 5s
 	sync
 	echo -e '\a' > /dev/console ; sleep $delay
