787bec4ac753b25f3047adae42a4a635d9fb002f
a-irs <a-irs@users.noreply.github.com>
2016-01-29 12:14:26 +0100

    add offsite backup back

--- bin/lib/backup.sh
+++ bin/lib/backup.sh
@@ -88,25 +88,3 @@ borg prune --verbose --stats --keep-within 1m "$REPO"
 
 header 2 "CLEANING UP BACKUP INFO FILES in $t"
 rm -rf "$t"
-
-
-############## offsite backup
-
-[[ $HOSTNAME != srv ]] && exit
-
-b=/media/data/backups
-
-rsync --size-only -av --delete -e "ssh -p 5522" "$b/borg/" alex@zshine.net:backups/borg-hosts
-
-REPO=ssh://alex@zshine.net:5522/home/alex/backups/borg-private
-DATE=$(date +%Y-%m-%d)_$(date +%H-%M-%S)
-BACKUP=( $b/host $b/studium $b/encrypt $b/encrypt-old $b/privat )
-
-borg init --encryption repokey "$REPO" || true
-borg create --verbose --stats --progress \
-    --one-file-system \
-    --compression lz4 \
-    --chunker-params 19,23,21,4095 \
-    "$REPO"::"$DATE" \
-    "${BACKUP[@]}"
-borg prune --verbose --stats --keep-within 1m "$REPO"
