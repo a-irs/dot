8612270b0d882ab801865eed703503c7598b5ae7
a-irs <a-irs@users.noreply.github.com>
2016-02-01 20:39:41 +0100

    firefox: private-tab css

--- config/awesome/awm-autostart.lua
+++ config/awesome/awm-autostart.lua
@@ -46,7 +46,7 @@ local programs = {
     'xset -dpms ; xset s off',
     hostname == "dell" and 'bash -c "if ! pgrep -x nm-applet; then nm-applet& fi"',
     hostname == "desktop" and 'pgrep -x mpd || mpd',
-    hostname == "desktop" and 'bash -c "pgrep -x x2godesktopsharing || x2godesktopsharing"',
+    hostname == "desktop" and 'bash -c "pgrep -x x2godesktopshar || x2godesktopsharing"',
     hostname == "desktop" and 'bash -c "pgrep -x synergyc || synergyc -d ERROR dell"',
     hostname == "desktop" and 'numlockx',
     hostname == "desktop" and 'sh -c \'sleep 10; sudo ethtool -s eth0 wol g\'',
