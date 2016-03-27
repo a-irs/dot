13f2fbefd447b01450ce92a4ef7c6792612e773b
a-irs <a-irs@users.noreply.github.com>
2016-03-27 17:53:56 +0200

    fix _theme.sh

--- _theme.sh
+++ _theme.sh
@@ -9,7 +9,7 @@ current_color=$(< "$cc_file")
 [[ ! $1 ]] && echo "USAGE: theme 2b4500" && exit 1
 
 cd "$this_dir"
-sed -i "s|$current_color|$1|g" "$this_dir/config/awesome/theme.lua" "$this_dir/config/sublime-text-3/Packages/User/Monokai Extended 2.tmTheme" "$this_dir/config/sublime-text-3/Packages/User/Monokai Extended 2 desaturated.tmTheme" "$this_dir/config/termite/config" "$this_dir/config/zathura/zathurarc" "$this_dir/newtab.html" "$this_dir/userChrome.css"
+sed -i "s|$current_color|$1|g" "$this_dir/config/awesome/theme.lua" "$this_dir/config/sublime-text-3/Packages/User/Monokai Extended 2.tmTheme" "$this_dir/config/sublime-text-3/Packages/User/Monokai Extended 2 desaturated.tmTheme" "$this_dir/config/termite/config" "$this_dir/config/zathura/zathurarc"
 echo "$1" > "$cc_file"
 
 killall -USR1 termite
