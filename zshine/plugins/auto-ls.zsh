e24860fd1a8f60d11d7de86adff685e86d94958d
a-irs <a-irs@users.noreply.github.com>
2016-04-20 21:06:01 +0200

    fix spotify

--- zshine/plugins/auto-ls.zsh
+++ zshine/plugins/auto-ls.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 chpwd() {
-    command ls --quoting-style=literal -F --color=auto --group-directories-first
+    command ls --quoting-style=literal -F --color=auto --group-directories-first 2> /dev/null
 }
 [[ "$TERM" != linux ]] && chpwd
