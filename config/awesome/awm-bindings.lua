47622015368fb5efd9fc6bfee510c92b6737d666
a-irs <a-irs@users.noreply.github.com>
2016-06-01 22:30:01 +0200

    fix

--- config/awesome/awm-bindings.lua
+++ config/awesome/awm-bindings.lua
@@ -337,9 +337,8 @@ end
 clientbuttons = awful.util.table.join(
     awful.button({     }, 1,      function(c) client.focus = c; c:raise() end),
     awful.button({ win }, 1,      awful.mouse.client.move),
-    awful.button({ win, alt }, 1, awful.mouse.client.dragtotag.border),
-    awful.button({ win }, 3,      awful.mouse.client.resize))
-
+    awful.button({ win }, 3,      awful.mouse.client.resize)
+)
 root.buttons(awful.util.table.join(
     awful.button({     }, 3,      function() awful.util.spawn("xfce4-appfinder --disable-server") end)
 ))
