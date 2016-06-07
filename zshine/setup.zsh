d220c963ecc6edbc0a05debb8da7b49c4e4f70a9
a-irs <a-irs@users.noreply.github.com>
2016-06-07 14:05:50 +0200

    add tty colors

--- zshine/setup.zsh
+++ zshine/setup.zsh
@@ -119,3 +119,23 @@ ZSHINE_PLUGINS=(
   zaw # better CTRL+R history search
 )
 for z in $ZSHINE_PLUGINS; do source "$ZSHINE_DIR/plugins/$z.zsh"; done
+
+# TTY colors // TODO parse from termite config
+if [ "$TERM" = "linux" ]; then
+    echo -en "\e]P1ff7563" #darkred
+    echo -en "\e]P2aae373" #darkgreen
+    echo -en "\e]P3fff073" #brown
+    echo -en "\e]P4b9d7f8" #darkblue
+    echo -en "\e]P5e2afdd" #darkmagenta
+    echo -en "\e]P6a8f6fd" #darkcyan
+    echo -en "\e]P7d7d7d7" #lightgrey
+    echo -en "\e]P86d7367" #darkgrey
+    echo -en "\e]P9ff7563" #red
+    echo -en "\e]PAaae373" #green
+    echo -en "\e]PBf2e575" #yellow
+    echo -en "\e]PCaed2fa" #blue
+    echo -en "\e]PDe2afdd" #magenta
+    echo -en "\e]PEa8f6fd" #cyan
+    echo -en "\e]PFd7d7d7" #white
+    clear
+fi
