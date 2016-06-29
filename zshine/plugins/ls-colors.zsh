f2bbc2c0c202eae810d8f69ebfcf0259b201c1b0
a-irs <a-irs@users.noreply.github.com>
2016-06-29 13:34:05 +0200

    MacOS fixes

--- zshine/plugins/ls-colors.zsh
+++ zshine/plugins/ls-colors.zsh
@@ -1,6 +1,6 @@
 if [[ -f "$ZSHINE_DIR/dircolors" ]]; then
-    [[ "$os" = Darwin ]] && eval $(gdircolors -b "$ZSHINE_DIR/dircolors")
     [[ "$os" = Linux  ]] && eval $(dircolors -b "$ZSHINE_DIR/dircolors")
+    [[ "$os" = Darwin  ]] && eval $(gdircolors -b "$ZSHINE_DIR/dircolors")
 fi
 
 autoload colors; colors;
@@ -11,3 +11,8 @@ zmodload -a colors
 zmodload -a autocomplete
 zmodload -a complist
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+if [[ $os == Darwin ]]; then
+    export CLICOLOR=1
+    zstyle ':completion:*:default' list-colors ''  
+fi
