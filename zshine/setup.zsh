7e177b5091e8395ebc55538f3485ed6644fe20a9
a-irs <a-irs@users.noreply.github.com>
2016-07-26 15:57:39 +0200

    restructure

--- zshine/setup.zsh
+++ zshine/setup.zsh
@@ -113,32 +113,14 @@ ZSHINE_PLUGINS=(
   auto-ls # launch "ls" when entering directory
   prompt-git # provides functions for a git-prompt
   completion # tweaks for TAB-completion
-  bindkey # bind keys for delete, history-search etc.
+  keys # bind keys for delete, history-search etc.
   fzf # CTRL+T for fuzzy-search of files
   # pwd-hell # show warning message when [[ pwd != readlink -f ./ ]]
   # tmux # autostart tmux
   # notify # notify-send after long command has been completed
   zshmarks # set bookmarks
   zaw # better CTRL+R history search
+  tty-colors # colors for $TERM=linux
 )
 for z in $ZSHINE_PLUGINS; do; source "$ZSHINE_DIR/plugins/$z.zsh"; done
 
-# TTY colors // TODO parse from termite config
-if [ "$TERM" = "linux" ]; then
-    echo -en "\e]P1ff7563" #darkred
-    echo -en "\e]P2aae373" #darkgreen
-    echo -en "\e]P3fff073" #brown
-    echo -en "\e]P4b9d7f8" #darkblue
-    echo -en "\e]P5e2afdd" #darkmagenta
-    echo -en "\e]P6a8f6fd" #darkcyan
-    echo -en "\e]P7d7d7d7" #lightgrey
-    echo -en "\e]P86d7367" #darkgrey
-    echo -en "\e]P9ff7563" #red
-    echo -en "\e]PAaae373" #green
-    echo -en "\e]PBf2e575" #yellow
-    echo -en "\e]PCaed2fa" #blue
-    echo -en "\e]PDe2afdd" #magenta
-    echo -en "\e]PEa8f6fd" #cyan
-    echo -en "\e]PFd7d7d7" #white
-    clear
-fi
