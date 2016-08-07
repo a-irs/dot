556713e92198156d9a6d7501c86723a1dcefa9b6
a-irs <a-irs@users.noreply.github.com>
2016-08-07 14:58:02 +0200

    setup

--- .setup.sh
+++ .setup.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+rm=0
+if [[ "$1" == rm ]]; then
+    rm=1
+fi
+
 os="$(uname)"
 
-print_error() { echo -e "\033[1;31m$*\033[0m"; }
-print_skip() { echo -e "\033[34m$*\033[0m"; }
-print_cleanup() { echo -e "\033[35m$*\033[0m"; }
-print_install() { echo -e "\033[33m$*\033[0m"; }
+print_error() { echo -e "\033[1;31m[ERROR] $*\033[0m"; }
+print_skip() { echo -e "\033[34m[SKIP] $*\033[0m"; }
+print_remove() { echo -e "\033[35m[REMOVE] $*\033[0m"; }
+print_install() { echo -e "\033[33m[INSTALL] $*\033[0m"; }
 
 if [[ "$os" = Darwin ]]; then
     this_dir="$(dirname "$(greadlink -f "$0")")"
@@ -15,7 +20,7 @@ fi
 
 rmlink() {
     dest=~/.$1
-    [[ -L "$dest" ]] && rm -f "$dest" && print_cleanup "[CLEANUP] removed ${dest/$HOME/\~}"
+    [[ -L "$dest" ]] && rm -f "$dest" && print_remove "${dest/$HOME/\~}"
 }
 
 mklink() {
@@ -26,7 +31,7 @@ mklink() {
             return
         fi
     elif [[ -e "$dest" ]]; then
-        print_error "[ERROR] ${dest/$HOME/\~} already exists and is no symlink!"
+        print_error "${dest/$HOME/\~} already exists and is no symlink!"
         return
     fi
 
@@ -37,10 +42,17 @@ mklink() {
     else
         ln --force --symbolic --no-target-directory --no-dereference "$this_dir/$1" "$dest"
     fi
-    print_install "[INSTALL] ${dest/$HOME/\~}"
+    print_install "${dest/$HOME/\~}"
 }
 
 install() {
+    if [[ "$rm" == 1 ]]; then
+        for f in "$@"; do
+            rmlink "$f"
+        done
+        return
+    fi
+
     if command -v "$1" > /dev/null 2>&1; then
         shift
         for f in "$@"; do
@@ -56,7 +68,11 @@ install() {
 
 install_always() {
     for f in "$@"; do
-        mklink "$f"
+        if [[ "$rm" == 1 ]]; then
+            rmlink "$f"
+        else
+            mklink "$f"
+        fi
     done
 }
 
@@ -93,3 +109,12 @@ if [[ -f /usr/bin/kupfer ]]; then
 else
     rm -f ~/.local/share/kupfer/plugins/recdirs.py
 fi
+
+if [[ -d ~/doc/zsh/ ]]; then
+    t=~/doc/zsh/$HOSTNAME
+    if [[ "$(readlink "$t")" != ~/.zsh_history ]]; then
+        ln -sf ~/.zsh_history "$t"
+        print_install "[INSTALL] ${t/$HOME/\~}"
+    fi
+fi
+
