cb9f0a515331adafbcd0e8313aa9329d55101df9
a-irs <a-irs@users.noreply.github.com>
2016-09-05 11:48:47 +0200

    virtualenvwrapper for debian

--- zshine/plugins/virtualenvwrapper.zsh
+++ zshine/plugins/virtualenvwrapper.zsh
@@ -1,10 +1,12 @@
 WORKON_HOME=~/dev/venvs
 
 [ -f /usr/bin/python2 ] && alias mkvirtualenv2="mkvirtualenv -p /usr/bin/python2"
+[ -f /usr/bin/python3 ] && alias mkvirtualenv3="mkvirtualenv -p /usr/bin/python3"
+
 [ -f /usr/bin/virtualenvwrapper_lazy.sh ] && source /usr/bin/virtualenvwrapper_lazy.sh
+[ -f /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh ] && source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
 
 pa="$WORKON_HOME/postactivate"
-
 if [[ -w "$pa" ]]; then
 
     cat <<EOF > $pa
