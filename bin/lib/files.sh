2604b8d04285da5d778057ae2cd4bbc443b88c79
a-irs <a-irs@users.noreply.github.com>
2016-09-27 16:56:44 +0200

    add files diff

--- bin/lib/files.sh
+++ bin/lib/files.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
 set -e
+export LC_ALL=C
+
+if (($# == 2)); then
+    set1=($1/*.bz2)
+    set2=($2/*.bz2)
+    for i in "${!set1[@]}"; do
+        diff -U 0 <(bzcat "${set1[$i]}" | sort) <(bzcat "${set2[$i]}" | sort) | tail -n +4 | grep -v '^@@ ' | sed 's|/media/||' | sed 's/^-/\x1b[31m- /;s/^+/\x1b[32m+ /;s/$/\x1b[0m/'
+    done
+    exit
+fi
 
 out="/root/files/$(date +%Y-%m-%d)"
 mkdir -p "$out"
 
-lsfiles() { find "$1" -type f | sort -hf | lzma > "$out/$(basename "$1").lzma"; }
+lsfiles() { find "$1" -type f | sort -hf | bzip2 > "$out/$(basename "$1").bz2"; }
 
 lsfiles /media/data1
 lsfiles /media/data2
