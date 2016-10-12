0225735439e4a4e689b6249127e0a4d6e3b80206
a-irs <a-irs@users.noreply.github.com>
2016-10-12 10:52:04 +0200

    default-sink

--- config/awesome/volume.lua
+++ config/awesome/volume.lua
@@ -2,17 +2,17 @@ local execute  = os.execute
 local volume   = {}
 
 function volume.toggle()
-    execute 'pactl set-sink-mute 0 toggle'
+    execute 'pactl set-sink-mute @DEFAULT_SINK@ toggle'
     pulsewidget.update()
 end
 
 function volume.increase()
-    execute 'pactl set-sink-volume 0 +1%'
+    execute 'pactl set-sink-volume @DEFAULT_SINK@ +1%'
     pulsewidget.update()
 end
 
 function volume.decrease()
-    execute 'pactl set-sink-volume 0 -1%'
+    execute 'pactl set-sink-volume @DEFAULT_SINK@ -1%'
     pulsewidget.update()
 end
 
