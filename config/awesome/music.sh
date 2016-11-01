2bcb555ca29e75893d0b1090132d17b00f1d848d
a-irs <a-irs@users.noreply.github.com>
2016-11-01 13:59:26 +0100

    fix html chars in music info

--- config/awesome/music.sh
+++ config/awesome/music.sh
@@ -31,8 +31,12 @@ color=${1:-eaa12e}
 out=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
 title=$(echo "$out" | grep -A 1 xesam:title | tail -1 | cut -d\" -f 2)
 title=${title//&/&amp;}
+title=${title//</&lt;}
+title=${title//>/&gt;}
 artist=$(echo "$out" | grep -A 2 xesam:artist | tail -1 | cut -d\" -f 2)
 artist=${artist//&/&amp;}
+artist=${artist//</&lt;}
+artist=${artist//>/&gt;}
 if [[ "$title" && "$artist" ]]; then
     echo " <span foreground='#$color'>♫ <b>$title</b> ($artist)</span> "
 fi
