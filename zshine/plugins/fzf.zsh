fa801f9f7310fa4fcd0482ff5e26c4847dde5f54
a-irs <a-irs@users.noreply.github.com>
2017-01-20 08:47:31 +0100

    update fzf

--- zshine/plugins/fzf.zsh
+++ zshine/plugins/fzf.zsh
@@ -1,11 +1,23 @@
 path+=("$ZSHINE_DIR/plugins/fzf/bin")
+if [[ ! $commands[fzf] ]]; then
+    "$ZSHINE_DIR/plugins/fzf/install" --bin
+fi
+
 manpath+=("$ZSHINE_DIR/plugins/fzf/man")
 source "$ZSHINE_DIR/plugins/fzf/shell/key-bindings.zsh"
 source "$ZSHINE_DIR/plugins/fzf/shell/completion.zsh"
 
-export FZF_DEFAULT_OPTS='--multi --no-mouse --cycle --height=20 --inline-info --preview="head -n 20 {}" --color=16'
+export FZF_COMPLETION_OPTS='--multi --no-mouse --cycle --height=20 --inline-info --preview="head -n 20 {}" --color=16'
 export FZF_COMPLETION_TRIGGER='#'
 
-_fzf_compgen_path() {
-  ag --hidden --ignore .git --ignore Cache --ignore cache --ignore .gem --ignore .npm -g "" "$1"
-}
+if [[ $commands[ag] ]]; then
+    _fzf_compgen_path() {
+        ag --hidden --g "" \
+            --ignore .git \
+            --ignore Cache \
+            --ignore cache \
+            --ignore .gem \
+            --ignore .npm \
+            "$1"
+    }
+fi
