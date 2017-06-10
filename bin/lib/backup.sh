a535220eda3b37b4fbd33eaceed9e98a5566c737
a-irs <a-irs@users.noreply.github.com>
2017-06-10 16:19:06 +0200

    update backup

--- bin/lib/backup.sh
+++ bin/lib/backup.sh
@@ -83,8 +83,8 @@ borg create \
     "$REPO"::"$DATE" \
     "${BACKUP[@]}"
 
-header 2 "PRUNING BACKUPS OLDER THAN 1 MONTH"
-borg prune --verbose --stats --list --keep-within 1m "$REPO"
+header 2 "PRUNING BACKUPS OLDER THAN 6 MONTHS"
+borg prune --verbose --stats --list --keep-within 6m "$REPO"
 
 header 2 "CLEANING UP BACKUP INFO FILES in $t"
 rm -rfv "$t"
