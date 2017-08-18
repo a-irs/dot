c4a48c75faaee4fc58dae42c565779cb0981d09e
a-irs <a-irs@users.noreply.github.com>
2017-08-18 10:16:28 +0200

    fix fzf

--- zshine/plugins/fzf.zsh
+++ zshine/plugins/fzf.zsh
@@ -7,9 +7,10 @@ manpath+=("$ZSHINE_DIR/plugins/fzf/man")
 source "$ZSHINE_DIR/plugins/fzf/shell/key-bindings.zsh"
 source "$ZSHINE_DIR/plugins/fzf/shell/completion.zsh"
 
-export FZF_DEFAULT_OPTS='--no-mouse --cycle --color=16 --height=20'
-export FZF_CTRL_R_OPTS='--exact'
-export FZF_COMPLETION_OPTS='--multi --preview="head -n 20 {}"'
+export FZF_DEFAULT_OPTS='--exact --no-mouse --cycle --color=16'
+export FZF_CTRL_R_OPTS='--exact --height=20'
+
+export FZF_COMPLETION_OPTS='--multi --preview="head -n 20 {}" --height=20 --no-exact'
 export FZF_COMPLETION_TRIGGER='#'
 
 if [[ $commands[ag] ]]; then
@@ -24,5 +25,5 @@ if [[ $commands[ag] ]]; then
     }
 fi
 
-FZF_ALT_C_OPTS='--preview="gls -1 --color=always --group-directories-first -F -- {}"'
+FZF_ALT_C_OPTS='--height=20 --preview="gls -1 --color=always --group-directories-first -F -- {}"'
 bindkey '^f' fzf-cd-widget
