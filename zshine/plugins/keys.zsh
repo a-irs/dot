52feebff72a9def57eebd2d1ffea3b093fa073b2
a-irs <a-irs@users.noreply.github.com>
2018-03-21 20:56:54 +0100

    New zsh key bindings

--- zshine/plugins/keys.zsh
+++ zshine/plugins/keys.zsh
@@ -1,22 +1,23 @@
 zmodload zsh/terminfo
 
-___prepend-sudo() {
-    [[ $BUFFER != "sudo "* ]] && BUFFER="sudo $BUFFER" && zle end-of-line
-}
-zle -N ___prepend-sudo
-bindkey "^[a" ___prepend-sudo
+# emacs bindings
+bindkey -e
 
 bindkey '^r' history-incremental-search-backward
-bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
-bindkey '^[[1;5C' forward-word                      # [Ctrl-RightArrow] - move forward one word
-bindkey '^[[1;5D' backward-word                     # [Ctrl-LeftArrow] - move backward one word
-bindkey "${terminfo[kcbt]}" reverse-menu-complete   # [Shift-Tab] - move through the completion menu backwards
+
+bindkey "^[[3~" delete-char  # DEL key
+bindkey '^[[1;5C' forward-word  # [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5D' backward-word  # [Ctrl-LeftArrow] - move backward one word
+bindkey -M emacs '^[[3;5~' kill-word  # CTRL+DEL
+bindkey -M emacs '^H' backward-kill-word  # CTRL+Backspace
+bindkey "${terminfo[kcbt]}" reverse-menu-complete  # [Shift-Tab] - move through the completion menu backwards
 bindkey "^Y" _expand_alias
 bindkey "^P" copy-prev-shell-word
-bindkey '^H' edit-command-line
+
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey '^G' edit-command-line
+
 bindkey -M menuselect '\e^M' accept-and-menu-complete # select multiple entries in TAB-completion with ESC+Return
 
 fg-toggle() { fg; }
