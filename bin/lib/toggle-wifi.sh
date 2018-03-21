b77d20efe8853c093be8779cfcfa367d1ad1d189
a-irs <a-irs@users.noreply.github.com>
2018-03-21 20:22:54 +0100

    Update for dell

--- bin/lib/toggle-wifi.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ssid=$(iwgetid --raw)
-
-if [[ "$ssid" == tpl ]]; then
-    nmcli connection up HITRON-ABE0 >/dev/null && echo "$(tput bold)now connected to 'HITRON-ABE0'"
-else
-    nmcli connection up tpl >/dev/null && echo "$(tput bold)now connected to 'tpl'"
-fi
