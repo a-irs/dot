8b2b18cd5f3edd0ecc83f468cdddd6820506a336
a-irs <a-irs@users.noreply.github.com>
2018-11-05 19:08:42 +0100

    Fix brightness/dpms stuff

--- bash/.bin/x1/bright.sh
+++ bash/.bin/x1/bright.sh
@@ -2,23 +2,36 @@
 
 set -eu
 
-current_brightness() {
-    xbacklight -get | cut -d . -f 1
+action=$1
+
+get_current_brightness() {
+    a=$(xbacklight -get | cut -d . -f 1)
+    # # round to fives
+    printf '%s\n' $((5*(a/5)))
 }
 
-if [[ "$1" == + ]]; then
-    xbacklight + 5
-elif [[ "$1" == - ]]; then
-    xbacklight - 5
+notify() {
+    icon=/usr/share/icons/Faba-Mono/48x48/notifications/notification-display-brightness.svg
+    notify-send -t 1000 -i "$icon" -- "Light" "$@"
+}
+
+backlight() {
+    xbacklight -time 100 "$@"
+}
+
+if [[ "$action" == + ]]; then
+    backlight -inc 5
+elif [[ "$action" == - ]]; then
+    backlight -dec 5
 fi
 
-cur=$(current_brightness)
+current=$(get_current_brightness)
 
 # limit to 1
-if [[ "$cur" == 0 ]]; then
-    xbacklight = 1
-    notify-send -t 1000 -i /usr/share/icons/Faba-Mono/48x48/notifications/notification-display-brightness.svg -- 1%
+if [[ "$current" == 0 ]]; then
+    backlight -set 1
+    notify 1%
 else
-    notify-send -t 1000 -i /usr/share/icons/Faba-Mono/48x48/notifications/notification-display-brightness.svg -- "$cur"%
+    notify "$current"%
 fi
 
