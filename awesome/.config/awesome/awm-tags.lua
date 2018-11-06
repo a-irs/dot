887b375c2a52da25120e5c8ee35ae8c3ca0f35ee
a-irs <a-irs@users.noreply.github.com>
2018-11-06 21:31:03 +0100

    Updates

--- awesome/.config/awesome/awm-tags.lua
+++ awesome/.config/awesome/awm-tags.lua
@@ -1,5 +1,5 @@
 local awful      = require 'awful'
 
 awful.screen.connect_for_each_screen(function(s)
-    awful.tag({1, 2, 3, 4, 5, 6, 7}, s, awful.layout.layouts[1])
+    awful.tag({" ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ "}, s, awful.layout.layouts[1])
 end)
