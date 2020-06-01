3f883a397a1ccbd9c1f9e095305a4010b2b20e20
a-irs <a-irs@users.noreply.github.com>
2020-06-01 20:39:40 +0200

    simplify term title

--- home/zshine/plugins/term-title.zsh
+++ home/zshine/plugins/term-title.zsh
@@ -1,29 +1,12 @@
 function title {
-  emulate -L zsh
   setopt prompt_subst
-
-  [[ "$EMACS" == *term* ]] && return
 
   # if $2 is unset use $1 as default
   # if it is set and empty, leave it as is
   : ${2=$1}
 
-  case "$TERM" in
-    cygwin|xterm*|putty*|rxvt*|ansi)
-      print -Pn "\e]2;$2:q\a" # set window name
-      print -Pn "\e]1;$1:q\a" # set tab name
-      ;;
-    screen*)
-      print -Pn "\ek$1:q\e\\" # set screen hardstatus
-      ;;
-    *)
-      if [[ -n "$terminfo[fsl]" ]] && [[ -n "$terminfo[tsl]" ]]; then
-        echoti tsl
-        print -Pn "$1"
-        echoti fsl
-      fi
-      ;;
-  esac
+  print -Pn "\e]2;$2:q\a" # set window name
+  print -Pn "\e]1;$1:q\a" # set tab name
 }
 
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
@@ -31,23 +14,12 @@ ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
 
 # Runs before showing the prompt
 function termsupport_precmd {
-  emulate -L zsh
-
-  if [[ "$DISABLE_AUTO_TITLE" == true ]]; then
-    return
-  fi
-
   title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
 }
 
 # Runs before executing the command
 function termsupport_preexec {
-  emulate -L zsh
   setopt extended_glob
-
-  if [[ "$DISABLE_AUTO_TITLE" == true ]]; then
-    return
-  fi
 
   # cmd name only, or if this is sudo or ssh, the next cmd
   local CMD=${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}
