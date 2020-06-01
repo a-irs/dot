f0c0273f49b1be6764384f7c0f8152e264188a05
a-irs <a-irs@users.noreply.github.com>
2020-06-01 23:44:12 +0200

    Improve tmux

--- home/zshine/plugins/tmux.zsh
+++ home/zshine/plugins/tmux.zsh
@@ -7,7 +7,7 @@
 # attach to latest detached session if available
 detached_session=$(tmux list-session -F '#{session_name}:#{session_attached}' | grep ':0' | tail -1)
 if [[ -n "$detached_session" ]]; then
-    exec tmux attach-session -t "${detached_session%:*}"
+    exec tmux attach-session -t "${detached_session%:*}" \; display "re-attached session"
 else
     exec tmux new-session
 fi
