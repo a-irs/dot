a34c865371c08a7aaf7ff6e96ada7bd3eac0b8bf
a-irs <a-irs@users.noreply.github.com>
2020-06-01 20:03:34 +0200

    tmux: ctrl+f

--- home/zshine/plugins/fzf.zsh
+++ home/zshine/plugins/fzf.zsh
@@ -22,8 +22,6 @@ for x in \
 done
 unset x
 
-bindkey '^f' fzf-cd-widget
-
 export FZF_DEFAULT_OPTS='--no-mouse --cycle --ansi --color=16 --tiebreak=end,begin,length --inline-info --tiebreak=length'
 
 export FZF_CTRL_R_OPTS='--height 70% --exact'
