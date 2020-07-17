a1a8678fa4a2883f424fe2f4e7c9e7f932157e1d
a-irs <a-irs@users.noreply.github.com>
2020-07-17 15:09:15 +0200

    Update

--- home/zshine/plugins/homebrew.zsh
+++ home/zshine/plugins/homebrew.zsh
@@ -3,14 +3,14 @@
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-bup() { su locai -c 'brew update && brew upgrade' && date +%s > ~/.brew-last-update; }
+bup() { su locai -c 'brew update && brew upgrade' && print -P '%D{%s}' > ~/.brew-last-update; }
 
 bup-check() {
     local days=$1
 
-    local bup_current=$(date +%s)
+    local bup_current_epoch=$(print -P '%D{%s}')  # zsh builtin
     local bup_last=$(< ~/.brew-last-update)
-    if (( bup_current - bup_last > 60 * 60 * 24 * days )); then
+    if (( bup_current_epoch - bup_last > 60 * 60 * 24 * days )); then
         printf "\n$BOLD_MAGENTA%s$RESET\n" "Homebrew not updated for more than ${days}d, please run bup."
     fi
 }
