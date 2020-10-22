bb74f429b516c5f42f7eb35b2daf83a529e0576b
a-irs <a-irs@users.noreply.github.com>
2020-10-22 17:45:48 +0200

    Updates for desk

--- home/config/awesome/awm-tags.lua
+++ home/config/awesome/awm-tags.lua
@@ -1,5 +1,13 @@
 local awful      = require 'awful'
 
-awful.screen.connect_for_each_screen(function(s)
-    awful.tag({" ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ "}, s, awful.layout.layouts[1])
-end)
+local default_layout = awful.layout.layouts[1]
+
+if hostname == "desk" then
+    awful.screen.connect_for_each_screen(function(s)
+        awful.tag({" ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ "}, s, default_layout)
+    end)
+else
+    awful.screen.connect_for_each_screen(function(s)
+        awful.tag({" ☐ ", " ☐ ", " ☐ ", " ☐ ", " ☐ "}, s, default_layout)
+    end)
+end
