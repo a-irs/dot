e53aebb3847a8e6a3070d8e3ecd945260e5a418f
a-irs <a-irs@users.noreply.github.com>
2020-10-22 17:48:54 +0200

    brightness control

--- home/config/awesome/awm-bindings.lua
+++ home/config/awesome/awm-bindings.lua
@@ -177,10 +177,14 @@ globalkeys = awful.util.table.join(
 
     awful.key({},        "XF86MonBrightnessUp", function() run_script("brightness +") end),
     awful.key({"Shift"}, "XF86MonBrightnessUp", function() run_script("brightness ++") end),
+    awful.key({"Ctrl", alt }, "Right",          function() run_script("brightness ++") end),
     awful.key({"Ctrl"},  "XF86MonBrightnessUp", function() run_script("brightness 100") end),
+    awful.key({"Ctrl", "Shift", alt }, "Right", function() run_script("brightness 100") end),
     awful.key({},        "XF86MonBrightnessDown", function() run_script("brightness -") end),
     awful.key({"Shift"}, "XF86MonBrightnessDown", function() run_script("brightness --") end),
+    awful.key({"Ctrl", alt }, "Left",             function() run_script("brightness --") end),
     awful.key({"Ctrl"},  "XF86MonBrightnessDown", function() run_script("brightness 1") end),
+    awful.key({"Ctrl", "Shift", alt }, "Left",    function() run_script("brightness 1") end),
 
     awful.key({},        "XF86AudioRaiseVolume", volume.increase),
     awful.key({"Shift"}, "XF86AudioRaiseVolume", volume.increase_10),
