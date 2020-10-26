c78a98aeb1373479a60807474b4c8fa1da4f8fc8
a-irs <a-irs@users.noreply.github.com>
2020-10-26 10:31:42 +0100

    cleanup

--- home/zshine/plugins/ssh-agent.zsh
+++ home/zshine/plugins/ssh-agent.zsh
@@ -3,13 +3,15 @@
 # only starts ssh-agent once and shares its ENV between shell sessions
 # inspired by: https://unix.stackexchange.com/a/217223
 
-if [[ ! -S ~/.ssh/ssh_auth_sock ]]; then
-    eval $(ssh-agent -t 3600 | grep -v '^echo ')
-    ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-    ln -sf /proc/"$SSH_AGENT_PID" ~/.ssh/ssh_agent_pid
-fi
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-export SSH_AGENT_PID=$(basename "$(readlink ~/.ssh/ssh_agent_pid)")
+ssh-agent-start() {
+    if [[ ! -S ~/.ssh/ssh_auth_sock ]]; then
+        eval $(ssh-agent -t 3600 | grep -v '^echo ')
+        ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+        ln -sf /proc/"$SSH_AGENT_PID" ~/.ssh/ssh_agent_pid
+    fi
+    export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+    export SSH_AGENT_PID=$(basename "$(readlink ~/.ssh/ssh_agent_pid)")
+}
 
 ssh-agent-kill() {
     eval $(ssh-agent -k)
@@ -17,3 +19,5 @@ ssh-agent-kill() {
     rm -vf ~/.ssh/ssh_agent_pid
     killall -u $USER ssh-agent 2>/dev/null || true
 }
+
+ssh-agent-start
