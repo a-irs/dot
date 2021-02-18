08f546517600f97d2400252466c55afc8ee40cb8
a-irs <a-irs@users.noreply.github.com>
2021-02-18 20:55:21 +0100

    Update

--- home/bin/lib/files.sh
+++ home/bin/lib/files.sh
@@ -6,8 +6,12 @@ export LC_ALL=C
 
 out=~/projects/filesgit
 
-lsfiles() { find "$1" -type f | sort -hf > "$out/$(basename "$1").txt"; }
-for d in /media/data?; do lsfiles "$d"; done
+if [[ "$#" -gt 0 && "$1" == commit ]]; then
+    lsfiles() { find "$1" -type f | sort -hf > "$out/$(basename "$1").txt"; }
+    for d in /media/data?; do lsfiles "$d"; done
 
-git -C "$out" add -v --all
-git -C "$out" commit -am "[automatic commit]" || true
+    git -C "$out" add -v --all
+    git -C "$out" commit -am "[automatic commit]" || true
+else
+    git -C "$out" log -p
+fi
