409f0bed2af8b2dc3eae096f3b59cb86fab964e5
a-irs <a-irs@users.noreply.github.com>
2021-03-30 11:49:57 +0200

    make venv python3 only

--- home/zshine/plugins/python.zsh
+++ home/zshine/plugins/python.zsh
@@ -1,25 +1,12 @@
 #!/usr/bin/env zsh
 
-VIRTUAL_ENV_DISABLE_PROMPT=1
-
 # default: stored in current directory
 export MYPY_CACHE_DIR=/tmp/mypy
 
 venv() {
     [[ -n "$VIRTUAL_ENV" ]] && { deactivate; return; }
     [[ $# != 1 ]] && { echo "venv <name>"; return 2; }
-    _venv_generic python3 "$1"
-}
-
-venv2() {
-    [[ -n "$VIRTUAL_ENV" ]] && { deactivate; return; }
-    [[ $# != 1 ]] && { echo "venv2 <name>"; return 2; }
-    _venv_generic python2 "$1"
-}
-
-_venv_generic() {
-    local py=$1
-    local e=$2
+    local e=$1
 
     if [[ -d "$e" ]]; then
         if [[ -f "$e/bin/activate" ]]; then
@@ -29,6 +16,6 @@ _venv_generic() {
             return 3
         fi
     else
-        "$py" -m venv "$e" && source "$e/bin/activate"
+        python3 -m venv "$e" && source "$e/bin/activate"
     fi
 }
