50110f4126eb357634267691e442c9fa7a052d0b
a-irs <a-irs@users.noreply.github.com>
2021-06-22 13:33:11 +0200

    upd

--- home/zshine/plugins/infra.zsh
+++ home/zshine/plugins/infra.zsh
@@ -1,6 +1,6 @@
 load() {
     for d in /srv/infra ~/git/a-irs/infra; do
-        [[ -O "$d" ]] && PATH="$d/ansible/bin:$d/docker:$d/docker-shot:$PATH"
+        [[ -O "$d" ]] && PATH="$d/ansible/bin:$d/docker/bin:$d/docker-shot/bin:$PATH"
     done
 }
 
