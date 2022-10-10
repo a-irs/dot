37f4114a7e73d54b92ba77ec7624f86f656a84f0
a-irs <a-irs@users.noreply.github.com>
2022-10-10 08:29:13 +0200

    upd

--- home/zshine/plugins/python.zsh
+++ home/zshine/plugins/python.zsh
@@ -10,21 +10,10 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 [[ "$commands[ipdb]" ]] && export PYTHONBREAKPOINT=ipdb.set_trace
 [[ "$commands[pudb]" ]] && export PYTHONBREAKPOINT=pudb.set_trace
 
-alias i=ipython
 
+# pipenv
+
+# or: pipenv shell
 venv() {
-    [[ -n "$VIRTUAL_ENV" ]] && { deactivate; return; }
-    [[ $# != 1 ]] && { echo "venv <name>"; return 2; }
-    local e=$1
-
-    if [[ -d "$e" ]]; then
-        if [[ -f "$e/bin/activate" ]]; then
-            source "$e/bin/activate"
-        else
-            echo "$e already exists and is not a venv"
-            return 3
-        fi
-    else
-        python3 -m venv "$e" && source "$e/bin/activate"
-    fi
+    source "$(pipenv --venv)/bin/activate"
 }
