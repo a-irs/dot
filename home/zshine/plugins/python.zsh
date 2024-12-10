84c54454d4a7d9053785d831422a5cfa15454685
a-irs <a-irs@users.noreply.github.com>
2024-12-10 13:28:49 +0100

    upd uv

--- home/zshine/plugins/python.zsh
+++ home/zshine/plugins/python.zsh
@@ -6,13 +6,13 @@ export MYPY_CACHE_DIR=~/.cache/mypy
 # do not prefix shell prompt with venv name
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
+[[ -e /opt/homebrew/etc/ca-certificates/cert.pem ]] && export REQUESTS_CA_BUNDLE=/opt/homebrew/etc/ca-certificates/cert.pem
+
 [[ "$commands[ipython]" ]] && export PYTHONBREAKPOINT=IPython.core.debugger.set_trace
 [[ "$commands[ipdb]" ]] && export PYTHONBREAKPOINT=ipdb.set_trace
 [[ "$commands[pudb]" ]] && export PYTHONBREAKPOINT=pudb.set_trace
 
-# hide message: "Pipenv found itself running within a virtual environment, so it will automatically use that environment, instead of creating its own for any project."
-export PIPENV_VERBOSITY=-1
-
+alias uv="uv --native-tls"  # use system CA store
 venv() {
     if [[ -n "$VIRTUAL_ENV" ]]; then
         deactivate
@@ -24,27 +24,33 @@ venv() {
         if ! [[ $REPLY =~ ^[Yy]$ ]]; then
             return
         fi
-        echo
+        printf '\n'
 
         mkdir -p .venv
         if [[ -f setup.py || -f pyproject.toml ]]; then
-            uv init --no-readme && rm -f hello.py
+            if [[ -f uv.lock ]]; then
+                uv sync --frozen
+            else
+                uv sync
+            fi
         else
-            uv sync --frozen
+            uv init --no-readme && rm -f hello.py
+            uv sync
         fi
 
         printf '\n%s\n' "----------"
         printf '%s\n' "Created virtualenv in $PWD/.venv"
         printf '%s\n' "----------"
 
-        echo '[[ -z "$VIRTUAL_ENV" ]] && source .venv/bin/activate' >> .env
-        echo '.env' >> .gitignore
+        printf '%s\n' '[[ -z "$VIRTUAL_ENV" ]] && source .venv/bin/activate' >> .env
+        printf '\n%s\n' '.env' >> .gitignore
     fi
 
     source .venv/bin/activate
 
-    echo "Cheatsheet:"
-    echo "uv add <packages>; uv remove <packages>"
-    echo "uv sync"
-    echo "uv run; uv run python"
+    printf '%s\n' "Cheatsheet"
+    printf '%s\n' "----------"
+    printf '%s\n' "uv add <packages>; uv remove <packages>"
+    printf '%s\n' "uv sync"
+    printf '%s\n' "uv run; uv run python"
 }
