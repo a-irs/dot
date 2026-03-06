4251210569de1e644aa5807566c7cdfbcbf2c2e3
a-irs <a-irs@users.noreply.github.com>
2026-03-06 13:44:53 +0100

    add json for vim

---
+++ home/config/nvim/lua/plugins/json.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "https://github.com/mogelbrod/vim-jsonpath",
+    init = function()
+      local rc = debug.getinfo(1).source:sub(2):gsub("%.lua", ".vim")
+      vim.cmd("source " .. rc)
+    end
+  }
+}
---
+++ home/config/nvim/lua/plugins/json.vim
@@ -0,0 +1,1 @@
+autocmd FileType json noremap <buffer> <silent> K :call jsonpath#echo()<CR>
